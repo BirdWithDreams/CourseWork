a7a80b164d4e72b053ac6d5b2efda2a8c1e6c20f
BirdWithDreams <87135024+BirdWithDreams@users.noreply.github.com>
2022-03-04 11:40:59 +0200

    Add first example off array

--- CourseWork/Array.cpp
+++ CourseWork/Array.cpp
@@ -1,1 +1,115 @@
 #include "Array.h"
+
+template<class T>
+Array<T>::Array()
+{
+	this->array = nullptr;
+	shape.push_back(0);
+	dims = 0;
+};
+
+template<class T>
+Array<T>::Array(T* begin, T* end)
+{
+	int n = end - begin;
+	this->array = new T[n];
+
+	for (int i = 0; i < n; i++)
+		this->array[i] = begin[i];
+
+	shape.push_back(n);
+	dims = 1;
+};
+
+template<class T>
+Array<T>::Array(T* arr, int n)
+{
+	this->array = new T[n];
+
+	for (int i = 0; i < n; i++)
+		this->array[i] = arr[i];
+
+	shape.push_back(n);
+	dims = 1;
+};
+
+template<class T>
+Array<T>::Array(T** arr, int n, int m)
+{
+	this->array = new T[n][m];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			this->array[i][j] = arr[i][j];
+
+	shape.push_back(n);
+	shape.push_back(m);
+	dims = 2;
+};
+
+template<class T>
+Array<T>::Array(int n)
+{
+	array = new T[n];
+
+	for (int i = 0; i < n; i++)
+		array[i] = 0;
+
+	shape.push_back(n);
+	dims = 1;
+};
+
+template<class T>
+Array<T>::Array(int n, int m)
+{
+	this->array = new T*[n];
+	for (int i = 0; i < n; i++)
+		array[i] = new T[m];
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			this->array[i][j] = 0;
+
+	shape.push_back(n);
+	shape.push_back(m);
+	dims = 2;
+};
+
+template<class T>
+void Array<T>::rand_uniform(T min, T max)
+{
+
+	switch (dims)
+	{
+	case 0:
+		break;
+
+	case 1:
+		for (int i = 0; i < shape[0]; i++)
+			array[i] = rand() / static_cast<double>(RAND_MAX) * (max - min) + min;
+		break;
+
+	case 2:
+		for (int i = 0; i < shape[0]; i++)
+			for (int j = 0; j < shape[1]; j++)
+				array[i][j] = rand() / static_cast<double>(RAND_MAX) * (max - min) + min;
+		break;
+	default:
+		break;
+	}
+};
+
+//template<class T>
+//Array<T> rand_uniform(T min, T max, int n)
+//{
+//	Array<T> array{ n };
+//	array.rand_uniform(min, max);
+//	return array;
+//}
+//
+//template<class T>
+//Array<T> rand_uniform(T min, T max, int n, int m)
+//{
+//	Array<T> array{ n, m };
+//	array.rand_uniform(min, max);
+//	return array;
+//}
--- CourseWork/Array.h
+++ CourseWork/Array.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <vector>
+
+template <class T>
 class Array
 {
+private:
+	T* array;
+	std::vector<int> shape;
+	int dims;
+
+public:
+	Array();
+	Array(T* begin, T* end);
+	Array(T* arr, int n);
+	Array(T** arr, int n, int m);
+	Array(int n);
+	Array(int n, int m);
+
+	void rand_uniform(T min, T max);
 };
 
+//template <class T>
+//Array<T> rand_uniform(T min, T max, int n);
+//
+//template <class T>
+//Array<T> rand_uniform(T min, T max, int n, int m);
